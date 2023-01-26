a1a1e7ace793c454ec99c4f6b1450c32235c528d
caiquebatalha <102067178+caiquebatalha@users.noreply.github.com>
2023-01-26 15:49:41 -0300

    Update conexaobd.php

--- Coat Trick 2.1/conexaobd.php
+++ Coat Trick 2.1/conexaobd.php
@@ -1,11 +1,18 @@
+<<<<<<< HEAD
 conexao php
 
+=======
+>>>>>>> 55b93c2ebe6b7b6b852e6f47b87d551b58ce815f
 <?php
 
     $servidor = "localhost";
     $usuario = "root";
     $senha = "";
+<<<<<<< HEAD
     $dbname = "cliente";
+=======
+    $dbname = "clientecoatt";
+>>>>>>> 55b93c2ebe6b7b6b852e6f47b87d551b58ce815f
 
     $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
 
