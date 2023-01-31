db902cd9fe8f523e3342cac3f86dbcc7c67bf4a5
daniel22055 <122386041+daniel22055@users.noreply.github.com>
2023-01-31 11:14:06 -0300

    Update registro.php

--- Coat Trick 2.1/registro.php
+++ Coat Trick 2.1/registro.php
@@ -6,15 +6,14 @@ if(isset($_POST['email'])) {
     $nome=$_POST{'nome'} ;
 $sobrenome = $_POST['sobrenome'];
 $email = $_POST['email'];
-$telefone = $_POST['telefone'];
 $cpf = $_POST['cpf'];
 $senha = password_hash ($_POST['senha'], PASSWORD_DEFAULT);
-$result_usuario = "INSERT INTO `cliente` (`nomeCliente`, `cpfCliente`,`idCliente`,'emailCliente','sobrenomeCliente','senhaCliente','confirmarsenhaCliente') VALUES (NULL, '$nome', '$sobrenome', '$email', '$telefone','$cpf','$senha')";
+$result_usuario = "INSERT INTO `cliente` (`nomeCliente`, `cpfCliente`,`idCliente`,'emailCliente','sobrenomeCliente','senhaCliente','confirmarsenhaCliente') VALUES (NULL, '$nome', '$sobrenome', '$email', '$cpf','$senha')";
 $resultado_usuario = mysqli_query($conn, $result_usuario);
 if (mysqli_insert_id($conn)){
-    header("Location: index.php");
+    header("Location: catalogoF.html");
 }else{
-    header("Location: index.php");
+    header("Location: catalogoF.html");
 }
 }
 
